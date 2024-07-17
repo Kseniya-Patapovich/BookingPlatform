2ddb0aef5ab5f442511a11dbaab4852f5ff46028
Kseniya Patapovich <ksuy.potapovich@bk.ru>
2024-07-17 13:23:52 +0300

    Created repositories: BookingRepository, VenueRepository

---
+++ src/main/java/com/bookingplatform/repository/BookingRepository.java
@@ -0,0 +1,12 @@
+package com.bookingplatform.repository;
+
+import com.bookingplatform.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findAllByVenueId(Long id);
+}
---
+++ src/main/java/com/bookingplatform/repository/VenueRepository.java
@@ -0,0 +1,11 @@
+package com.bookingplatform.repository;
+
+import com.bookingplatform.model.Venue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VenueRepository extends JpaRepository<Venue, Long> {
+}
