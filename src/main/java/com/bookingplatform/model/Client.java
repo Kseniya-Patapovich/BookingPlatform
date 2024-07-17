ba089b5811dc62f65801c0cff10440ba14b19a2d
Kseniya Patapovich <ksuy.potapovich@bk.ru>
2024-07-17 10:53:50 +0300

    Created models: Client, Property

---
+++ src/main/java/com/bookingplatform/model/Client.java
@@ -0,0 +1,31 @@
+package com.bookingplatform.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDate;
+
+@Entity(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
+}
---
+++ src/main/java/com/bookingplatform/model/Property.java
@@ -0,0 +1,36 @@
+package com.bookingplatform.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@Entity(name = "properties")
+public class Property {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String address;
+
+    @Column
+    private BigDecimal price;
+
+    @Column
+    private String phone;
+
+    @OneToMany(mappedBy = "property", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Client> clients;
+}
